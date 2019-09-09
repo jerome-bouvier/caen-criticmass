63a06a61de7fac6fe02420dfb91ef5b34d8d1036
Jerome-bouvier <moeb.bzhash@gmail.com>
2019-09-09 14:00:29 +0200

    1st plot

--- caen-cm.py
+++ caen-cm.py
@@ -1,20 +1,41 @@
 # stats velo @ Caen
 
-import pprint
+import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import matplotlib.pyplot as plt
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+# Preps google
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive'
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'client_secret.json', scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("Copy of CompteursCaen").sheet1
 
-pp = pprint.PrettyPrinter()
+# Date
+date_du_jour = datetime.datetime.today().strftime('%d-%m-%Y')
+sheet.update_cell(3, 2, date_du_jour)
+
+# Values
+dates = sheet.col_values(1)
+dates = dates[5:]
+values = sheet.col_values(2)
+values = values[5:]
+
+# Graph
+plt.plot(dates, values)
+plt.xlabel('date')
+plt.ylabel('Nombres de cyclistes')
+plt.title('Evolution du nombre de cyclistes @ Caen depuis avril 2018')
+
 
 def main():
+    plt.show()
 
-    result = sheet.get_all_records()
-    pp.pprint(result)
 
+if __name__ == "__main__":
+    main()
