3524fddd43e5d9564fea9a09bf1d21148f7d99d6
Jerome-bouvier <moeb.bzhash@gmail.com>
2019-09-10 11:39:06 +0200

    tweaks

--- caen-cm.py
+++ caen-cm.py
@@ -4,6 +4,7 @@ import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Preps google
 scope = [
@@ -14,24 +15,28 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(
     'client_secret.json', scope)
 client = gspread.authorize(creds)
 
+print('Loading datas from Google sheets...')
 sheet = client.open("Copy of CompteursCaen").sheet1
 
 # Date
 date_du_jour = datetime.datetime.today().strftime('%d-%m-%Y')
 sheet.update_cell(3, 2, date_du_jour)
+sheet.update_cell(2, 2, '01-01-2019')
 
-# Values
+# Retrieve dates & values
 dates = sheet.col_values(1)
 dates = dates[5:]
+
 values = sheet.col_values(2)
 values = values[5:]
 datas = dict(zip(dates, values))
 
 # Graph
 plt.plot(datas.keys(), datas.values())
+plt.gcf().autofmt_xdate()
 plt.xlabel('date')
-plt.ylabel('Nombres de cyclistes')
-plt.title('Evolution du nombre de cyclistes @ Caen depuis avril 2018')
+plt.ylabel('Nombre de cyclistes')
+plt.title('Evolution du nombre de cyclistes @ Caen en 2019')
 
 
 def main():
